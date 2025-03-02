a8ab628eef6f2eab999e45cb5b40c460a4b31040
jainilpatel308 <jainilpatel308@gmail.com>
2025-03-02 13:47:50 -0500

    Spring boot app

---
+++ src/main/java/com/example/ems/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.example.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.out.println(dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
